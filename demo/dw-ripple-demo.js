4634590286e4b25c56f8cd26e391f5a26bf598ec
shakil-mansuri-dream <53848663+shakil-mansuri-dream@users.noreply.github.com>
2020-12-02 14:35:12 +0530

    fix: CHANGE: remove mwc-ripple and provide own/custom implementation

--- demo/dw-ripple-demo.js
+++ demo/dw-ripple-demo.js
@@ -30,6 +30,8 @@ export class DwRippleDemo extends LitElement {
         }
 
         .demo-box {
+          position: relative;
+          overflow: hidden;
           width: 96px;
           height: 96px;
           border: 1px solid gray;
@@ -42,7 +44,7 @@ export class DwRippleDemo extends LitElement {
         }
 
         .demo-box.custom {
-          --mdc-theme-on-surface: #c11e5c;
+          --dw-ripple-color: #c11e5c;
         }
 
         mwc-formfield {
