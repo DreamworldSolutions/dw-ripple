4ecff6baf059a1c37d32a4fc1a91e20b01a98bec
Shakil Mansuri <shakil.mansuri@dreamworldsolutions.com>
2020-12-03 17:00:20 +0530

    fix: pointer-events none for ripple.

--- dw-ripple.js
+++ dw-ripple.js
@@ -16,6 +16,7 @@ export class DwRipple extends LitElement {
     return [
       css`
       :host {
+        pointer-events: none;
         position: absolute;
         border-radius: 50%;
         transform: scale(0);
